797d108c5ed3f42e163830b866defba69fe7c75e
Francisco Pereira Alvarado <gammafp@gmail.com>
2024-12-05 12:59:53 +0100

    init

---
+++ tool/easy-resizer/precache-manifest.7bb123f6fb472cd39c4e5bce148f5553.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbefb70769bae2379db76f4262858461",
+    "url": "/tool/easy-resizer/index.html"
+  },
+  {
+    "revision": "32b7d8bbbb3973ad1669",
+    "url": "/tool/easy-resizer/static/css/2.495df41a.chunk.css"
+  },
+  {
+    "revision": "d7ffd5c1063b9200e24d",
+    "url": "/tool/easy-resizer/static/css/main.73c1380d.chunk.css"
+  },
+  {
+    "revision": "32b7d8bbbb3973ad1669",
+    "url": "/tool/easy-resizer/static/js/2.902cc2cc.chunk.js"
+  },
+  {
+    "revision": "fdb6324462cbd891a4b10db70c510210",
+    "url": "/tool/easy-resizer/static/js/2.902cc2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ffd5c1063b9200e24d",
+    "url": "/tool/easy-resizer/static/js/main.ccb03d9c.chunk.js"
+  },
+  {
+    "revision": "3fad2eef510128aa8054",
+    "url": "/tool/easy-resizer/static/js/runtime-main.f86fb322.js"
+  },
+  {
+    "revision": "d7e60f9d1433a45ed71817f6d23abeca",
+    "url": "/tool/easy-resizer/static/media/material_icons.d7e60f9d.woff2"
+  }
+]);
